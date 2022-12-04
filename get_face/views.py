de77d94572de92d41e218f05fcc6b9578661a973
нефот <_@email.com>
2022-12-04 21:18:09 +0300

    df

--- get_face/views.py
+++ get_face/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def geting(request):
+
+    return render(request, 'blog/header.html')
--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from portfolio import views
-
+from portfolio.views import user
+from get_face.views import geting
 urlpatterns = [
-
+    path('get/', geting, name='get'),
     path('blog/', include('blog.urls')),
-    path('', views.user, name='home'),
+    path('', user, name='home'),
     path('admin/', admin.site.urls),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
